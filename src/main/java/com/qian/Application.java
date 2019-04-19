e7194f81dad4bffc0a16ff20d911c3c613669887
kdichen <kdichen@163.com>
2019-04-19 22:45:11 +0800

    整合mybatis

--- src/main/java/com/qian/Application.java
+++ src/main/java/com/qian/Application.java
@@ -3,15 +3,17 @@ package com.qian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author : ChenQian
  * @date : 2019/4/19 21:57
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.qian.mapper")
 @ComponentScan(basePackages = {"com.qian"})
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
